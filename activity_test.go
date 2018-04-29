d73e09f8a5b02673c6e058958c9fbda0c8e12b77
Rahul Nair <rnair@tibco.com>
2018-04-29 23:05:15 +0530

    test

--- activity_test.go
+++ activity_test.go
@@ -54,7 +54,7 @@ func TestEval(t *testing.T) {
 	tc.SetInput("accountSid", "AC8112820c2969c0b9ba6abac8ee6a4062")
 	tc.SetInput("authToken", "f2542c88dbfb58c494e642bf10af4140")
 	tc.SetInput("urlString", "https://api.twilio.com/2010-04-01/Accounts/")
-	tc.SetInput("msgData[1]", "Hello from flogo")
+	tc.SetInput("[]msgData", [4]string{"Rahul", "Dipesh", "Jay", "Aditya"})
 	tc.SetInput("to", "+919177623444")
 	tc.SetInput("from", "+14437433811")
 
@@ -62,7 +62,7 @@ func TestEval(t *testing.T) {
 	fmt.Println(tc.GetOutput("result"))
 	//check result attr
 
-	result := "Message sent successfully to:+919177623444"
-	assert.Equal(t, result, "Message sent successfully to:+919177623444")
+	res := tc.GetOutput("result")
+	assert.Equal(t, res, [4]string{"Rahul", "Dipesh", "Jay", "Aditya"})
 
 }
